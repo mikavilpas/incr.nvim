8f75168b7f7df037e8ab0cf3b9ec3a206db5ae7e
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2025-06-08 22:16:41 +0300

    Initial version

---
+++ README.md
@@ -0,0 +1,47 @@
+# incr.nvim
+
+This plugin uses nvim-treesitter to select nodes incrementally. You can start
+selection in normal mode by clicking `tab` and then repeatedly clicking `tab`
+select more scope. You can use `s-tab` to decrement selection.
+
+## Installation
+
+Using lazyvim.
+
+```lua
+  {
+    'daliusd/incr.nvim',
+    config = true,
+  },
+```
+
+If you want to override default keys then use something like this:
+
+```lua
+  {
+    'daliusd/incr.nvim',
+    opts = {
+        incr_key = '<tab>', -- increment selection key
+        decr_key = '<s-tab>', -- decrement selection key
+    },
+  }
+```
+
+## History
+
+nvim-treesitter main branch drop increment selection feature. From
+https://github.com/nvim-treesitter/nvim-treesitter/issues/4767#issue-1698676665:
+
+> incremental-selection mostly served as a proof-of-concept for
+> non-highlighting uses of tree-sitter; if people are actively using it, they
+> should consider moving it to a separate plugin (or seeing if textobjects
+> don't serve this purposes even better); alternatively rewrite as simple node
+> and scope textobjects;
+
+This is basically gist of it. Initially I have tried to move this to my nvim
+configuration as simple solution, but found out that simple solution was lacking
+something as tree-sitter methods are not necessary doing what their
+documentation says.
+
+This does not match original nvim-treesitter incremental-selection feature - if
+you are missing something feel free to create issue or even better PR.
---
+++ lua/incr/init.lua
@@ -0,0 +1,114 @@
+local M = {}
+
+_G.selected_nodes = {} ---@type TSNode[]
+
+local function get_node_at_cursor()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local row = cursor[1] - 1
+  local col = cursor[2]
+
+  local root_parser = vim.treesitter.get_parser()
+  if not root_parser then return end
+
+  root_parser:parse { vim.fn.line "w0" - 1, vim.fn.line "w$" }
+  local lang_tree = root_parser:language_for_range({ row, col, row, col })
+
+  return lang_tree:named_node_for_range({ row, col, row, col }, { ignore_injections = false })
+end
+
+local function select_node(node)
+  if not node then return end
+  local start_row, start_col, end_row, end_col = node:range()
+
+  local last_line = vim.api.nvim_buf_line_count(0)
+  local end_row_pos = math.min(end_row + 1, last_line)
+  local end_col_pos = end_col
+
+  if end_row + 1 > last_line then
+    local last_line_text = vim.api.nvim_buf_get_lines(0, last_line - 1, last_line, true)[1]
+    end_col_pos = #last_line_text
+  end
+
+  vim.fn.setpos("'<", { 0, start_row + 1, start_col + 1, 0 })
+  vim.fn.setpos("'>", { 0, end_row_pos, end_col_pos, 0 })
+
+  vim.cmd('normal! gv')
+end
+
+M.setup = function(config)
+  local incr_key = config.incr_key and config.incr_key or '<tab>'
+  local decr_key = config.decr_key and config.decr_key or '<s-tab>'
+
+  vim.keymap.set({ "n" }, incr_key, function()
+    _G.selected_nodes = {}
+
+    local current_node = get_node_at_cursor()
+    if not current_node then return end
+
+    table.insert(_G.selected_nodes, current_node)
+    select_node(current_node)
+  end, { desc = "Select treesitter node" })
+
+
+  vim.keymap.set("x", incr_key, function()
+    if #_G.selected_nodes == 0 then
+      return
+    end
+
+    local current_node = _G.selected_nodes[#_G.selected_nodes]
+
+    if not current_node then return end
+
+    local node = current_node
+    local root_searched = false
+    while true do
+      local parent = node:parent()
+      if not parent then
+        if root_searched then
+          return
+        end
+
+        local root_parser = vim.treesitter.get_parser()
+        if root_parser == nil then return end
+        root_parser:parse { vim.fn.line "w0" - 1, vim.fn.line "w$" }
+
+        local range = { node:range() }
+        local current_parser = root_parser:language_for_range(range)
+
+        if root_parser ~= current_parser then
+          local parser = current_parser:parent()
+          if parser == nil then return end
+          current_parser = parser
+        end
+
+        if root_parser == current_parser then
+          root_searched = true
+        end
+
+        parent = current_parser:named_node_for_range(range)
+        if parent == nil then return end
+      end
+
+      local range = { node:range() }
+      local parent_range = { parent:range() }
+      if not vim.deep_equal(range, parent_range) then
+        table.insert(_G.selected_nodes, parent)
+        select_node(parent)
+        return
+      end
+      node = parent
+    end
+  end, { desc = "Increment selection" })
+
+  vim.keymap.set("x", decr_key, function()
+    if #_G.selected_nodes > 1 then
+      table.remove(_G.selected_nodes)
+      local current_node = _G.selected_nodes[#_G.selected_nodes]
+      if current_node then
+        select_node(current_node)
+      end
+    end
+  end, { desc = "Decrement selection" })
+end
+
+return M
