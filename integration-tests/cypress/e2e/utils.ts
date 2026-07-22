8d351cac30746a45f1d6c0be8e450d029e0dd4d4
Mika Vilpas <mika.vilpas@gmail.com>
2026-07-22 08:15:51 +0300

    chore: use `@mikavilpas/oxfmt-config` package for oxfmt configuration

--- integration-tests/cypress/e2e/utils.ts
+++ integration-tests/cypress/e2e/utils.ts
@@ -2,13 +2,7 @@ import { flavors } from "@catppuccin/palette"
 import { rgbify, textIsVisibleWithBackgroundColor } from "@tui-sandbox/library"
 
 export const isHighlighted = (text: string): Cypress.Chainable<JQuery> =>
-  textIsVisibleWithBackgroundColor(
-    text,
-    rgbify(flavors.macchiato.colors.surface1.rgb),
-  )
+  textIsVisibleWithBackgroundColor(text, rgbify(flavors.macchiato.colors.surface1.rgb))
 
 export const isNotHighlighted = (text: string): Cypress.Chainable<JQuery> =>
-  textIsVisibleWithBackgroundColor(
-    text,
-    rgbify(flavors.macchiato.colors.base.rgb),
-  )
+  textIsVisibleWithBackgroundColor(text, rgbify(flavors.macchiato.colors.base.rgb))
